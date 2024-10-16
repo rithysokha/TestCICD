1c9cc198493603c9c628e4a34cefaf3a4d871d2c
Sokha Rithy <124172865+rithysokha@users.noreply.github.com>
2024-10-16 13:54:01 +0700

    test controller

--- src/main/java/com/student/testcicd/Tcontroller.java
+++ src/main/java/com/student/testcicd/Tcontroller.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Tcontroller {
     @GetMapping
     public String hello(){
-        return "Hello";
+        return "<h1>CI/CD success</h1>";
     }
 }
